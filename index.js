c82a17e0524029fe46ef8a53ebd245bde495fc45
Justin Odhiambo <justinsibudiodhiambo@gmail.com>
2023-10-02 16:13:45 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code in this file!
+
+const currentUser = ('Grace Hopper');
+
+
+const myString = 'template literal';
+
+const myNumber = 10;
+
+const myBoolean = false;
+
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser}!`.slice(0, 1)!`;
+
